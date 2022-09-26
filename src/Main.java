5d9cfb1a7758b2ca6a5cab818ff4ae2b4428e8d9
Matheus Ferreira <matheus.aferreira@mercadolivre.com>
2022-09-26 20:10:19 -0300

    feat: added Stream examples

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
+        List<String> countries = Stream.of("Brasil", "Argentina")
+                .map(country -> country.toUpperCase())
+                .collect(Collectors.toList());
 
-
+        countries.forEach(System.out::println);
     }
 }
---
+++ src/lambdaStreams/Main.java
@@ -0,0 +1,45 @@
+package lambdaStreams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        // Exemplo de Stream
+        List<String> countries = Stream.of("Brasil", "Argentina")
+                .map(String::toUpperCase) // equivalente a country -> country.toUpperCase
+                .collect(Collectors.toList());
+
+        countries.forEach(System.out::println);
+        System.out.println(countries);
+
+        // Exemplo 2 de Stream
+        List<Integer> numbers = Arrays.asList(4, 2, 6, 9, 8, 5, 1);
+        List<Integer> filteredNumbers = numbers.stream().filter(number -> number > 5).collect(Collectors.toList());
+        System.out.println("filteredNumbers" + filteredNumbers);
+
+        // Exemplo 3 de Stream
+        Integer result = numbers.stream().mapToInt(number -> number * number).sum();
+        System.out.println(result);
+
+        // Exemplo 4 Stream
+        Map<String, List<Integer>> group = numbers
+                .stream()
+                .collect(Collectors.groupingBy(number -> {
+                    if (number % 2 == 0) return "Par";
+                    else return "Impar";
+                }));
+
+        System.out.println("groupingBy: " + group);
+
+        // Exemplo 5 Stream
+        String joining = numbers.stream()
+                .map(String::valueOf) // Transformei em strings, pois o joining só aceita string
+                .collect(Collectors.joining(" - ")); // parametro que separa cada item da lista (opcional)
+
+        System.out.println("joining: " + joining);
+    }
+}
