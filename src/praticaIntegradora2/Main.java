f7be57dd75d575ad7a7fd269f8f7195c8461eaaa
Matheus Ferreira <matheus.aferreira@mercadolivre.com>
2022-09-26 21:21:20 -0300

    feat: integrative pratice 2 - exercise 2

--- src/praticaIntegradora2/Main.java
+++ src/praticaIntegradora2/Main.java
@@ -1,7 +1,23 @@
 package praticaIntegradora2;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ola");
+        Vehicle vehicle1 = new Vehicle("Fiesta", "Ford", 1000);
+        Vehicle vehicle2 = new Vehicle("Focus", "Ford", 1200);
+        Vehicle vehicle3 = new Vehicle("Explorer", "Ford", 2500);
+        Vehicle vehicle4 = new Vehicle("Uno", "Fiat", 500);
+        Vehicle vehicle5 = new Vehicle("Cronos", "Fiat", 1000);
+        Vehicle vehicle6 = new Vehicle("Torino", "Fiat", 1250);
+        Vehicle vehicle7 = new Vehicle("Aveo", "Chevrolet", 1250);
+        Vehicle vehicle8 = new Vehicle("Spin", "Chevrolet", 2500);
+        Vehicle vehicle9 = new Vehicle("Corola", "Toyota", 1200);
+        Vehicle vehicle10 = new Vehicle("Fortuner", "Toyota", 3000);
+        Vehicle vehicle11 = new Vehicle("Logan", "Renault", 950);
+
+        List<Vehicle> vehicles =Arrays.asList(vehicle1, vehicle2, vehicle3, vehicle4, vehicle5, vehicle6, vehicle7, vehicle8, vehicle9, vehicle10, vehicle11)
+        Garage garage = new Garage(vehicles);
     }
 }
